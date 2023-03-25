33b6c8283087098e2bf42299eec79925e41f0658
Clement Dike <dikeclementu@gmail.com>
2023-03-24 18:55:27 -0600

    public subnet fix & fmt

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,3 +1,6 @@
 provider "aws" {
-  profile = "devops"
+ access_key = "ASIAVV2TE3SV6DXEI6LN"
+ secret_key = "gOiAgBpG9p/qcZxIH2aiX4B3JF+k1Nr54QuxxMlz"
+ token = "FwoGZXIvYXdzECoaDLmQ7lQg6yec3LtM9SK1ARwOyOEGKi4hO5e/RIOxZ8YcIrLLiNlc0edE0mtGl0sg1SkXhFwg61YDNCNug5GdfVTcsVa+UqTcqF1Tjt+m6ZPkn0zwYJ1kEcrB2IDW88WKkZbjAyfa7svi5YdHwITt/BzTWXEQM1m9QqkqWVzwl6fpRXbc8SaJ/h/WqV+r2/fTk5Lux0t0Lya5qRq1Q1Wt8Xo7ZfHTpmFwvSVZLq2YBDfyO2A+MtenkeO42mR42L8D2b/7PPooiYr5oAYyLWohz5q88/1T27OzRqLSwMf7QkIdcBSKD7CwUL3igMNqLfduzUf4SQaAvBpHvQ=="
+ region = "us-east-1"
 }
