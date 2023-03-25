a2dd68e42145960e590d94d445f8f7046b44f6a1
Clement Dike <dikeclementu@gmail.com>
2023-03-24 18:30:32 -0600

    variables (vpc)

---
+++ Terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "environment" {
+  type = string
+}
+
+################### VPC ####################
+variable "vpc_create" {
+  type = bool
+  default = false
+}
+
+variable "vpc_name" {
+  type = string
+  default = ""
+}
+
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type =  list(string)
+  default = []
+}
+
+variable "public_subnets" {
+  type =  list(string)
+  default = []
+}
+
+variable "tags" {
+  type = map(string)
+  default = {}
+}
